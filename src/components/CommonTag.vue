ca2e6163bd056b1694c1cd15257e1305d2aacc94
huanghaibo <1123595386@qq.com>
2023-09-12 08:19:58 +0800

    新增样式和弹窗

--- src/components/CommonTag.vue
+++ src/components/CommonTag.vue
@@ -2,7 +2,7 @@
     <div class="tabs">
         <el-tag v-for="(item, index) in tags" :key="item.path" :closable="item.name !== 'home'"
             :effect="$route.name === item.name ? 'dark' : 'plain'" @click="changeMenu(item)"
-            @close="handleClose(item, index)">
+            @close="handleClose(item, index)" size="small">
             {{ item.label }}
         </el-tag>
     </div>
@@ -28,16 +28,17 @@ export default {
             this.$router.push({ name: item.name })
         },
         handleClose(item, index) {
-
+            //原来的索引
+            const length = this.tags.length - 1
             this.closeTag(item)
-            const length = this.tags.length
-            //删除后跳转逻辑
+            //删除后跳转逻辑·
             //删除的不是当前路由
             if (item.name !== this.$route.name) {
                 return
             }
             console.log(index);
             //删除的是最后一个 
+            //判断规则就是删除的那个索引值是否和最后一个值的索引相等
             if (index === length) {
                 this.$router.push({
                     name: this.tags[index - 1].name
@@ -53,4 +54,13 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.tabs {
+    padding: 20px;
+
+    .el-tag {
+        margin-right: 15px;
+        cursor: pointer;
+    }
+}
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        我是 User
+    <div class="manage">
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+        <div class="manage-header">
+            <el-button @click="dialogVisible = true" type="primary" size="small">
+                +新增
+            </el-button>
+        </div>
     </div>
 </template>
 <script>
@@ -8,7 +19,9 @@ export default {
     data() {
         return {
             property: 'value',
+            dialogVisible: false,
         };
     },
 }
 </script> 
+<style lang="less" scoped></style>
